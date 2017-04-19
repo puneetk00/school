4c8d2f17f2606b6a6da9a549af877232aa58319c
puneetk00 <puneetk00@gmail.com>
2017-04-19 09:11:46 +0530

    Create dateloop.php

---
+++ dateloop.php
@@ -0,0 +1,10 @@
+<?php
+
+$begin = new DateTime( '2017-04-22' );
+$end = new DateTime( '2017-05-10' );
+
+$interval = DateInterval::createFromDateString('1 day');
+$period = new DatePeriod($begin, $interval, $end);
+
+foreach ( $period as $dt )
+  echo $dt->format( "Y-m-d H:i:s\n" ) . "<br>";
